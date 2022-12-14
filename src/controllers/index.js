239ab7625d619326647f1e50721ce70f67ca4aba
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 16:31:24 -0300

    feat(controllers): :sparkles: implements middleware to create new user

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const login = require('./login');
+const userController = require('./user');
 
 module.exports = {
   login,
+  userController,
 };
---
+++ src/controllers/user.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const { userService } = require('../services');
+const { mapError } = require('../utils/errorMap');
+
+const create = async (req, res) => {
+  try {
+    const { email, displayName, password, image } = req.body;
+    const userData = { email, displayName, password, image };
+    const { type, message } = await userService.create(userData);
+
+    if (type) {
+      return res.status(mapError(type)).json({ message });
+    }
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+
+    return res.status(201).json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  create,
+};
