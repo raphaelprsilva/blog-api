ad97fdec8a7d27e2ded05e0f6357a569feb3d1a1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 16:33:22 -0300

    feat(routes): :sparkles: add POST route `/user`

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { loginRouter } = require('./routes');
+const { loginRouter, userRouter } = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/login', loginRouter);
+app.use('/user', userRouter);
 
 module.exports = app;
