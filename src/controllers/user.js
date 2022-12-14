3d2cb1f3eda4cfb06bfda2d78d36f18673e8ab9b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 19:25:21 -0300

    feat(controllers): :sparkles: implements `getById` method
    this middleware allows returns 200 status and user data on success, or 404 status and a message error when user is not found

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -37,7 +37,25 @@ const getAll = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await userService.getById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
