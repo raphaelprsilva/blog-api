321518b3fa61ea9069039228c00a42968e149168
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 19:17:17 -0300

    feat(routes): :sparkles: implements GET `/user` route
    this route allows new user on logged user to list all user data

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const { userController } = require('../controllers');
+const validateJWT = require('../middlewares/auth/validateJWT');
 
 const router = express.Router();
 
 router.post('/', userController.create);
+router.get('/', validateJWT, userController.getAll);
 
 module.exports = router;
