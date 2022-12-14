2a3377a682b8fa2160e4698d56484ecfa64fb6e5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 19:15:38 -0300

    feat(controllers): :sparkles: implements `getAll` method
    this method returns status 200 and users data on success or status 500 and json message with error

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -26,6 +26,18 @@ const create = async (req, res) => {
   }
 };
 
+const getAll = async (req, res) => {
+  try {
+    const users = await userService.getAll();
+
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
