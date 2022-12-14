397634b424d93432cf18a139f67b57fc8ee5863c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 15:35:29 -0300

    fix(models): :bug: remove `name` property and comment `User` association
    `name` property was incorrect and `User` association was causing a server error

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       image: DataTypes.STRING,
       displayName: DataTypes.STRING,
-      name: DataTypes.STRING,
       password: DataTypes.STRING,
     },
     {
@@ -18,9 +17,9 @@ module.exports = (sequelize, DataTypes) => {
     },
   );
 
-  User.associate = (models) => {
-    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'posts' });
-  };
+  // User.associate = (models) => {
+  //   User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'posts' });
+  // };
 
   return User;
 };
