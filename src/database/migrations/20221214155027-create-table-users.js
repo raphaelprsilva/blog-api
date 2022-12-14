4138488290d352c2c42b4354a537cbcf0776b2e2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-14 13:03:18 -0300

    feat: :sparkles: add user migration

---
+++ src/database/migrations/20221214155027-create-table-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    }),
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
+};
