571ed78fbd1d52ea9e2261b319ac214d3a048015
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-15 15:06:51 -0300

    feat(controllers): :sparkles: implements `getPostById` middleware
    this middleware is reponsable for returns post data or an error if it is not found

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -33,7 +33,24 @@ const getAllPosts = async (_req, res) => {
   }
 };
 
+const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await postService.getPostById(id);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post does not exist' });
+    }
+
+    return res.status(200).json(post);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   createPost,
   getAllPosts,
+  getPostById,
 };
