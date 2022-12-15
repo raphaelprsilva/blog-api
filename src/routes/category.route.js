3ff3e45e492b8b64bc81d3283bb1f8ad3780e2a8
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-15 08:39:34 -0300

    feat(routes): :sparkles: implements GET `/categories` route

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -6,5 +6,6 @@ const validateJWT = require('../middlewares/auth/validateJWT');
 const router = express.Router();
 
 router.post('/', validateJWT, categoryController.create);
+router.get('/', validateJWT, categoryController.getAll);
 
 module.exports = router;
