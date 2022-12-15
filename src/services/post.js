435a8ae4e2424c423485a946d74d59a1cc87ed87
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-15 14:59:21 -0300

    feat(services): :sparkles: implements `getAllPosts` function
    this function allows to get all posts with respective data based on models associations

--- src/services/post.js
+++ src/services/post.js
@@ -48,6 +48,17 @@ const createPost = async ({ title, content, categoryIds, userEmail }) => {
   }
 };
 
+const getAllPosts = async () => {
+  const posts = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  return posts;
+};
+
 module.exports = {
   createPost,
+  getAllPosts,
 };
