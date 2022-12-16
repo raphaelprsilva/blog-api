e343ed2e56039e7d5bd5603702a34051acade701
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-16 10:34:28 -0300

    feat(routes): :sparkles: implements GET `/post/search?q=:searchTerm` route

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/', validateJWT, postController.createPost);
 router.get('/', validateJWT, postController.getAllPosts);
+router.get('/search', validateJWT, postController.searchPosts);
 router.get('/:id', validateJWT, postController.getPostById);
 router.put('/:id', validateJWT, postController.updatePost);
 router.delete('/:id', validateJWT, postController.deletePost);
